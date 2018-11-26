1b11d38ef0858d4e8cb3ca75a8af57086609d1d5
ShujiOba <shuji365630@gmail.com>
2018-11-27 03:21:07 +0900

    [update] docker-compose等を修正

--- api/wait.sh
+++ api/wait.sh
@@ -2,11 +2,6 @@
 
 echo "Waiting for mysql"
 
-until mysql --host="$MYSQL_HOST" --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" &> /dev/null
-do
-    echo "waiting..."
-    sleep 1
-done
+sleep 60
 
-echo "MySQL is up"
 ./api
